066316ed3b8f4fc88aeadae45e163676dd3d9589
User <muzu04994@gmail.com>
2024-05-04 19:16:58 +0530

    initial

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# MongoDB connection
+client = MongoClient('mongodb://localhost:27017/')
+db = client['medical_lab']
+lab_results_collection = db['lab_results']
+
 @app.route('/')
 def index():
-    return 'Welcome to the Medical Lab Report Management System!'
+    return render_template('index.html')
+
+@app.route('/lab_results')
+def get_lab_results():
+    results = lab_results_collection.find()
+    lab_results = []
+    for result in results:
+        lab_results.append({
+            'patient_name': result['patient_name'],
+            'test_name': result['test_name'],
+            'result': result['result']
+        })
+    return jsonify({'lab_results': lab_results})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
