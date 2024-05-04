6e86845dbf43ee09a3dfc813fb92c4148960e0fd
User <muzu04994@gmail.com>
2024-05-04 20:12:34 +0530

    initial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -12,17 +12,23 @@ lab_results_collection = db['lab_results']
 def index():
     return render_template('index.html')
 
-@app.route('/lab_results')
+@app.route('/lab_results', methods=['GET'])
 def get_lab_results():
-    results = lab_results_collection.find()
-    lab_results = []
-    for result in results:
-        lab_results.append({
-            'patient_name': result['patient_name'],
-            'test_name': result['test_name'],
-            'result': result['result']
-        })
-    return jsonify({'lab_results': lab_results})
+    report_id = request.args.get('r_id')
+
+    if report_id:
+        result = lab_results_collection.find_one({'r_id': report_id})
+        if result:
+            lab_result = {
+                'patient_name': result['patient_name'],
+                'test_name': result['test_name'],
+                'result': result['result']
+            }
+            return jsonify({'lab_result': lab_result}), 200
+        else:
+            return jsonify({'error': 'Report not found'}), 404
+    else:
+        return jsonify({'error': 'Report ID parameter is required'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
